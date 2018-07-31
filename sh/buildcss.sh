5e627b846035c0fc14ed61b5527f84f24bd6a9bf
zhangwentao <taowenzhang@gmail.com>
2018-07-31 23:54:13 +0800

    update

--- sh/buildcss.sh
+++ sh/buildcss.sh
@@ -1,4 +1,4 @@
 LESS_PATH=../_stasrc/less/
-CSS_FILE=/Users/wentao/Desktop/dist/blog.wentao.me/static/css/main.css
+CSS_FILE=../static/css/main.css
 #echo '' > $CSS_FILE
-lessc  -x ${LESS_PATH}main.less >> $CSS_FILE
+lessc  -x ${LESS_PATH}main.less > $CSS_FILE
